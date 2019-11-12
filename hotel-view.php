b059682e4b23a81bc968d521b5724f027f4d41ec
Marwen - JAMMELI <marwen.jammeli@gmail.com>
2019-11-12 13:34:50 +0100

    Add files via upload

--- hotel-view.php
+++ hotel-view.php
@@ -1,5 +1,4 @@
-
-<script >
+<script>
 jQuery(function($){
     "use strict";
 
@@ -11,12 +10,156 @@ jQuery(function($){
     =======================================*/
 
     SLZ.mainFunction = function(){
+        $('.wrapper-journey').slick({
+            infinite: false,
+            slidesToShow: 6,
+            slidesToScroll: 6,
+            autoplay: false,
+            speed: 700,
+            dots: true,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 1201,
+                    settings: {
+                        slidesToShow: 5,
+                        slidesToScroll: 5
+                    }
+                },
+                {
+                    breakpoint: 1025,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 4
+                    }
+                },
+                {
+                    breakpoint: 769,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        dots: true,
+                        arrows: false,
+                    }
+                },
+                {
+                    breakpoint: 481,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        dots: true,
+                        arrows: false,
+                    }
+                },
+                {
+                    breakpoint: 381,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        dots: true,
+                        arrows: false,
+                    }
+                }
+            ]
+        });
+
+        $('.slider-for.group1').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            asNavFor: '.slider-nav.group1'
+        });
+        $('.slider-nav.group1').slick({
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            asNavFor: '.slider-for.group1',
+            arrows: false,
+            infinite: true,
+            // centerMode: true,
+            focusOnSelect: true,
+            responsive: [
+                {
+                    breakpoint: 1201,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 381,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1
+                    }
+                },
+            ]
+        });
+        $('.slider-for.group2').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            asNavFor: '.slider-nav.group2'
+        });
+        $('.slider-nav.group2').slick({
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            asNavFor: '.slider-for.group2',
+            arrows: false,
+            infinite: true,
+            focusOnSelect: true,
+            responsive: [
+                {
+                    breakpoint: 1201,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 1
+                    }
+                },
+            ]
+        });
+        $('.slider-for.group3').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            asNavFor: '.slider-nav.group3'
+        });
+        $('.slider-nav.group3').slick({
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            asNavFor: '.slider-for.group3',
+            arrows: false,
+            infinite: true,
+            focusOnSelect: true,
+            responsive: [
+                {
+                    breakpoint: 1201,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 1
+                    }
+                },
+            ]
+        });
+
+        $('.btn-book-tour').click(function(event) {
+            /* Act on the event */
+            event.preventDefault();
+            $(this).parents('.timeline-content').find('.timeline-book-block').toggleClass('show-book-block');
+        });
+
 
         /*Google map*/
-		<?php $gpp=explode(',', $restotparametres['gmap']);?>
+		<?php if(isset($res['gmap']) && $res['gmap']!='') $gpp=explode(',', $res['gmap']);
+		else $gpp=explode(',', $restotparametres['gmap']);
+		
+		?>
 		
         var myLatLng = {lat: <?php echo $gpp[0];?>, lng: <?php echo $gpp[1];?>};
         var markerLatLng = {lat: <?php echo $gpp[0];?>, lng: <?php echo $gpp[1];?>};
+		
         var customMapType = new google.maps.StyledMapType(
             [
                 {
@@ -97,7 +240,7 @@ jQuery(function($){
                 map: map,
                 animation:google.maps.Animation.BOUNCE, 
                 icon: image,
-                title: 'RBS TRAVEL'
+                title: '<?php echo  $res['nom']?>'
             });
 
             var contentString = '';
@@ -108,7 +251,7 @@ jQuery(function($){
 
             marker.addListener('click', function() {
                // infowindow.open(map, marker);
-               $('.btn-open-map').parents('.wrapper-info').toggle(200);
+               $('.btn-open-map').parents('.map-info').toggle(400);
                // $('#googleMap').css('pointer-events', 'none');
             });
         }
@@ -116,7 +259,7 @@ jQuery(function($){
 
         $('.btn-open-map').click(function(event) {
             /* Act on the event */
-            $(this).parents('.wrapper-info').toggle(400);
+            $(this).parents('.map-info').toggle(400);
             $('#googleMap').css('pointer-events', 'auto');
             if($(window).width() > 462) {
                 mapProp = {
